1e504823a5a8f2642f1c266786c4cb2c37b448e3
Krystof Pesek <krystof.pesek@gmail.com>
2023-02-13 11:39:49 +0100

    Fixing paths

--- process-openmedia-data/openmedia-minify.sh
+++ process-openmedia-data/openmedia-minify.sh
@@ -5,7 +5,7 @@ YEAR=`date +%Y`
 WEEK=`date +%V`
 
 filename=$LOGS_FOLDER/process-openmedia-data/openmedia_minify_`date +%Y-%m-%d`.log
-/root/openmedia-minify/openmedia-minify -i $ANNOVA/$YEAR/W$WEEK -o $ANNOVA/$YEAR > $filename
+/root/openmedia-minify/openmedia-minify -i $ANNOVA/Rundowns/$YEAR/W$WEEK -o $ANNOVA/Temporary/Minify/$YEAR > $filename
 
 
 
