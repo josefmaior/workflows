710e30831106d4177a6c12728fc8110df1919a9f
Krystof Pesek <krystof.pesek@gmail.com>
2023-03-06 10:33:04 +0100

    obsolete definition fix

--- analyze-irozhlas/scrape_publicistika.sh
+++ analyze-irozhlas/scrape_publicistika.sh
@@ -2,11 +2,11 @@
 #
 #
 
-function scrape_main {
+scrape_main () {
   curl -s -L https://plus.rozhlas.cz/hlavni-zpravy-rozhovory-a-komentare-5997846 | grep "b-022__block" | grep href | grep publicistika | grep -v tag | awk -F'"' '{print "https://plus.rozhlas.cz"$8}' > /tmp/links.txt
 }
 
-function scrape_factbox {
+scrape_factbox () {
   for i in `cat /tmp/links.txt` ;
     do curl -s -L "$i" | grep factbox | lynx --dump -assume_charset=utf-8 --display_charset=utf-8 -stdin  ;
   done
