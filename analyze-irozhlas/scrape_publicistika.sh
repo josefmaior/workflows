4c62f8e89af1a878c9bf725e0cb962874405759c
Krystof Pesek <krystof.pesek@gmail.com>
2023-03-08 12:06:11 +0100

    #10 update script, merge

--- analyze-irozhlas/scrape_publicistika.sh
+++ analyze-irozhlas/scrape_publicistika.sh
@@ -6,14 +6,16 @@ function scrape_main {
   for i in `cat /tmp/links.txt` ; do
     curl -s -L "$i" | grep "h1" | grep -v display | lynx --dump -assume_charset=utf-8 --display_charset=utf-8 -stdin  | grep -v "'' + '' +" ;
     curl -s -L "$i" | grep factbox | lynx --dump -assume_charset=utf-8 --display_charset=utf-8 -stdin  ;
-    echo -e "\n"
-    echo -e "[link]($i)"
-    echo -e "\n"
+    printf "$i\n\n"
   done
 }
 
 function scrape_pro_a_proti {
   curl -s -L https://plus.rozhlas.cz/pro-a-proti-6482952 | grep "b-022__block" | grep -v tag |  awk -F'"' '{print "https://plus.rozhlas.cz"$8}' > /tmp/links.txt
+
+  for i in `cat /tmp/links.txt` ; do
+    curl -s -L "$i" | lynx --dump -assume_charset=utf-8 --display_charset=utf-8 -stdin ; 
+  done
 }
 
 function scrape_dvacet_min_rz {
@@ -26,6 +28,7 @@ function scrape_interview_plus {
 
 ############## RUN HERE ##################
 scrape_main || exit 1
+
 
 # need more parsing
 #scrape_pro_a_proti
