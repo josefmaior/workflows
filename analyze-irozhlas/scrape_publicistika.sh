cda20dfedf301924a4e6be8b9fcba73cff5f8ae0
Krystof Pesek <krystof.pesek@gmail.com>
2023-03-08 11:27:20 +0100

    update scripts

--- analyze-irozhlas/scrape_publicistika.sh
+++ analyze-irozhlas/scrape_publicistika.sh
@@ -3,8 +3,12 @@
 function scrape_main {
   curl -s -L https://plus.rozhlas.cz/hlavni-zpravy-rozhovory-a-komentare-5997846 | grep "b-022__block" | grep href | grep publicistika | grep -v tag | awk -F'"' '{print "https://plus.rozhlas.cz"$8}' > /tmp/links.txt
 
-  for i in `cat /tmp/links.txt` ;
-    do curl -s -L "$i" | grep factbox | lynx --dump -assume_charset=utf-8 --display_charset=utf-8 -stdin  ;
+  for i in `cat /tmp/links.txt` ; do
+    curl -s -L "$i" | grep "h1" | grep -v display | lynx --dump -assume_charset=utf-8 --display_charset=utf-8 -stdin  | grep -v "'' + '' +" ;
+    curl -s -L "$i" | grep factbox | lynx --dump -assume_charset=utf-8 --display_charset=utf-8 -stdin  ;
+    echo -e "\n"
+    echo -e "[link]($i)"
+    echo -e "\n"
   done
 }
 
@@ -25,6 +29,8 @@ function scrape_interview_plus {
 
 scrape_main
 
-scrape_pro_a_proti
-scrape_dvacet_min_rz
-scrape_interview_plus
+
+# need more parsing
+#scrape_pro_a_proti
+#scrape_dvacet_min_rz
+#scrape_interview_plus
