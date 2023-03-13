6bc34da5486b423cede45122bfba17f0d7ad491f
Krystof Pesek <krystof.pesek@gmail.com>
2023-03-13 10:19:54 +0100

    #11 update data zverejneni

--- analyze-irozhlas/scrape_publicistika.sh
+++ analyze-irozhlas/scrape_publicistika.sh
@@ -4,7 +4,12 @@ scrape_main(){
   curl -s -L https://plus.rozhlas.cz/hlavni-zpravy-rozhovory-a-komentare-5997846 | grep "b-022__block" | grep href | grep publicistika | grep -v tag | awk -F'"' '{print "https://plus.rozhlas.cz"$8}' > /tmp/links.txt;
 
   for i in `cat /tmp/links.txt` ; do
+
+    # hlavicka nadpis
     curl -s -L "$i" | grep "h1" | grep -v display | lynx --dump -assume_charset=utf-8 --display_charset=utf-8 -stdin  | grep -v "'' + '' +" ;
+    # datum zverejneni
+    curl -s -L "$i" | grep node-block__block--date -A 3 | lynx --dump -assume_charset=utf-8 --display_charset=utf-8 -stdin ; 
+    # hoste sedivy-box
     curl -s -L "$i" | grep factbox | lynx --dump -assume_charset=utf-8 --display_charset=utf-8 -stdin  ;
     printf "$i\n\n";
   done
