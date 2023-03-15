64ed9b2c982599ca25da39b9281effc26704e0dc
Krystof Pesek <krystof.pesek@gmail.com>
2023-03-15 10:37:11 +0100

    #11 update running script

--- analyze-irozhlas/scrape_publicistika.sh
+++ analyze-irozhlas/scrape_publicistika.sh
@@ -15,27 +15,14 @@ scrape_main(){
   done
 }
 
-scrape_pro_a_proti(){
-  curl -s -L https://plus.rozhlas.cz/pro-a-proti-6482952 | grep "b-022__block" | grep -v tag |  awk -F'"' '{print "https://plus.rozhlas.cz"$8}' > /tmp/links.txt;
-  for i in `cat /tmp/links.txt` ; do
-    curl -s -L "$i" | lynx --dump -assume_charset=utf-8 --display_charset=utf-8 -stdin ; 
-  done
+scrape_rest(){
+  ./irozhlas_porady_hoste_scraper/irozhlas_porady_hoste_scraper
 }
-
-scrape_dvacet_min_rz(){
-  curl -s -L https://plus.rozhlas.cz/dvacet-minut-radiozurnalu-5997743 | grep "b-022__block" | grep -v tag |  awk -F'"' '{print "https://plus.rozhlas.cz"$8}' > /tmp/links.txt
-}
-
-scrape_interview_plus(){
-  curl -s -L https://plus.rozhlas.cz/interview-plus-6504167 | grep "b-022__block" | grep -v tag |  awk -F'"' '{print "https://plus.rozhlas.cz"$8}' > /tmp/links.txt
-}
-
 
 ############## RUN HERE ##################
+
 scrape_main || exit 1
 
 
-# need more parsing
-#scrape_pro_a_proti
-#scrape_dvacet_min_rz
-#scrape_interview_plus
+scrape_rest || exit 1
+
